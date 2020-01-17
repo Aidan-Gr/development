c95271f130b6eeebdc3242b44d37b35c32bf7fcf
Aidan-Gr <60008528+Aidan-Gr@users.noreply.github.com>
2020-01-17 16:31:59 +0100

    Add files via upload

---
+++ shopify.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  console.log('Thanks for reading WeeklyHow\'s Tutorials!');
+}
+</script>
+</head>
+
+<body>
+
+myFunction()
+
+</body>
+</html>
